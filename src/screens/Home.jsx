5000b28b897c2445197c17ed3961c3c2fadffabe
Anil Maurya <anil.empi@gmail.com>
2020-12-01 21:10:11 +0530

    vector-icons,react-native-paper,imagepicker

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,15 +3,41 @@ import {StyleSheet, Text, View} from 'react-native';
 import {container} from '../styles';
 import Button from 'react-native-paper/src/components/Button';
 
+import ImagePicker from 'react-native-image-picker';
+
 function Home() {
+  const handleCamera = () => {
+    const config = {
+      mediaType: 'photo',
+      includeBase64: false,
+      maxHeight: 300,
+      maxWidth: 300,
+    };
+    ImagePicker.launchCamera(config, (response) => {
+      console.log(response);
+    });
+  };
+
+  const handleGallery = () => {
+    const config = {
+      mediaType: 'photo',
+      includeBase64: false,
+      maxHeight: 200,
+      maxWidth: 200,
+    };
+    ImagePicker.launchImageLibrary(config, (response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => console.log('Pressed..')}>
-        Press Me
+      <Button icon="camera" mode="contained" onPress={handleCamera}>
+        Take Picture
+      </Button>
+      <Button icon="image" mode="outlined" onPress={handleGallery}>
+        Gallery
       </Button>
     </View>
   );
